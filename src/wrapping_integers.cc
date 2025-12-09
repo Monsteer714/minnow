32d86bba58a8561c557563076e3d8ba9d7d8871b
Monsteer714 <1633492517@qq.com>
2025-12-09 18:19:39 +0800

    Passed all the tests for Wrap32
    记住无符号数的距离对比 注意下溢问题

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-#include "debug.hh"
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,12 +7,33 @@ const uint64_t MOD = 4294967296;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32(zero_point + n);
+  return Wrap32( zero_point + n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  const auto converted_zero_point = static_cast<uint64_t> (zero_point.raw_value_);
-  const auto converted_this_value = static_cast<uint64_t> (this->raw_value_);
-  return checkpoint + ((converted_this_value - converted_zero_point + MOD) % MOD);
+  const auto converted_zero_point = static_cast<uint64_t>( zero_point.raw_value_ );
+  const auto converted_this_value = static_cast<uint64_t>( this->raw_value_ );
+
+  const uint64_t offset = (converted_this_value - converted_zero_point + MOD) % MOD;
+  const uint64_t mid = (( checkpoint / MOD ) * MOD) + offset;
+  const uint64_t down = (mid >= MOD) ? mid - MOD : UINT64_MAX;
+  const uint64_t up = mid + MOD;
+
+  auto abs_diff = [&](uint64_t a, uint64_t b) -> uint64_t {
+    return (a > b) ? (a - b) : (b - a);
+  };
+
+  auto diff1 = abs_diff(checkpoint, down);
+  auto diff2 = abs_diff(checkpoint, mid);
+  auto diff3 = abs_diff(checkpoint, up);
+  uint64_t res = 0;
+  if (diff1 <= diff2 && diff1 <= diff3) {
+    res = down;
+  } else if (diff2 <= diff1 && diff2 <= diff3) {
+    res = mid;
+  } else {
+    res = up;
+  }
+  return res;
 }
